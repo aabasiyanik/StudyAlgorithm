5876aaed9ea305416c573686653d2619e22ef275
Ahmed Abasiyanik <aabasiyanik@Ahmeds-MacBook-Pro.local>
2023-06-19 20:17:40 -0500

    validate subsequence problem solved

---
+++ validateSubsequence.py
@@ -0,0 +1,21 @@
+def is_valid_subsequence(arr, subsequence):
+    # Initialize pointers for both lists
+    arr_index = 0
+    seq_index = 0
+    
+    # Iterate over the sequence list until either list reaches its end
+    while arr_index < len(arr) and seq_index < len(subsequence):
+        # If the current elements in both lists are equal, move the subsequence pointer
+        if arr[arr_index] == subsequence[seq_index]:
+            seq_index += 1
+        
+        # Move the sequence pointer in any case
+        arr_index += 1
+    
+    # Check if the subsequence pointer has reached the end of the subsequence list
+    return seq_index == len(subsequence)
+
+array = [5, 56, 12, 59, 102, 3, 67]
+sub = [56, 59, 67]
+
+print(is_valid_subsequence(array, sub))
